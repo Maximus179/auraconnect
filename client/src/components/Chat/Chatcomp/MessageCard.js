da525ae3d15f5a08a2219a13d68aaddd119a6ca0
Maximus179 <osheenarora17@gmail.com>
2021-07-12 12:34:17 +0530

    Update MessageCard.js

--- client/src/components/Chat/Chatcomp/MessageCard.js
+++ client/src/components/Chat/Chatcomp/MessageCard.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
 import { auth } from "./../../../firebase";
+import { formatRelative } from 'date-fns';
 
-const MessageCard = ({ message, handleDelete }) => {
-  const { id, text, uid, createdAt, email } = message;
+const MessageCard = ({ message}) => {
+  const { text, uid, createdAt, email } = message;
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
   const [showActionsButtons, setShowActionsButtons] = useState(false);
   const toggleCard = () => {
     setShowActionsButtons(!showActionsButtons);
   };
+
+  const formatDate = date => {
+    let formattedDate = '';
+    if (date) {
+      // Convert the date in words relative to the current date
+      formattedDate = formatRelative(date, new Date());
+      // Uppercase the first letter
+      formattedDate =
+        formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    }
+    return formattedDate;
+  };
+
 
   return (
     <>
@@ -16,8 +30,12 @@ const MessageCard = ({ message, handleDelete }) => {
           <div className="email">
             <p>{email}</p>
           </div>
-          <div className="createdAt">
-            <p>{createdAt}</p>
+          <div className="email">
+            <p>{createdAt?.seconds ? (
+            <span className="text-gray-500 text-xs">
+              {formatDate(new Date(createdAt.seconds * 1000))}
+            </span>
+          ) : null}</p>
           </div>
           <div className="text">
             <p>{text}</p>
