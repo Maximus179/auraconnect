74c89fcc93febe0505a4f34ca618aab74612c17e
Maximus179 <73634323+Maximus179@users.noreply.github.com>
2021-07-12 11:44:15 +0530

    Update CallPage.js

--- client/src/components/CallPage/CallPage.js
+++ client/src/components/CallPage/CallPage.js
@@ -243,7 +243,7 @@ const CallPage = () => {
   return (
     <div>
       <div className="callpage-container">
-        <video className="video-container" src=""></video>
+        <video className="video-container" src="" muted="muted"></video>
 
         <CallPageHeader
           isMessenger={isMessenger}
